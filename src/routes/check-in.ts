91c4319c619ae32d275c53736d715d3873aec109
Guilherme Henrique Crozariol <guicrozariol@gmail.com>
2024-04-20 14:36:20 -0300

    feat(route): create check-in route

---
+++ src/routes/check-in.ts
@@ -0,0 +1,41 @@
+import { prisma } from '@/lib/prisma'
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+export async function checkIn(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().get(
+    '/attendees/:attendeeId/check-in',
+    {
+      schema: {
+        params: z.object({
+          attendeeId: z.coerce.number().int(),
+        }),
+        response: {
+          201: z.null(),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { attendeeId } = request.params
+
+      const attendeeCheckIn = await prisma.checkIn.findUnique({
+        where: {
+          attendeeId,
+        },
+      })
+
+      if (attendeeCheckIn) {
+        throw new Error('Check-in already made.')
+      }
+
+      await prisma.checkIn.create({
+        data: {
+          attendeeId,
+        },
+      })
+
+      return reply.status(201).send()
+    },
+  )
+}
--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,7 @@ import { createEvent } from './routes/create-event'
 import { registerForEvent } from './routes/register-for-event'
 import { getEvent } from './routes/get-event'
 import { getAttendeeBadge } from './routes/get-attendee-badge'
+import { checkIn } from './routes/check-in'
 
 const app = fastify()
 
@@ -37,6 +38,7 @@ app.register(createEvent)
 app.register(registerForEvent)
 app.register(getEvent)
 app.register(getAttendeeBadge)
+app.register(checkIn)
 
 app
   .listen({ port: 3333 })
